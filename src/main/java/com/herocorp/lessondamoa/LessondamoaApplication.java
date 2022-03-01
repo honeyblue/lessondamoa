671bd1f838975d7587a0a6e2b5a9a2f33b1d1572
honeyblue <dev.honeyblue@gmail.com>
2022-03-01 11:20:21 +0900

    Initial commit

---
+++ src/main/java/com/herocorp/lessondamoa/LessondamoaApplication.java
@@ -0,0 +1,13 @@
+package com.herocorp.lessondamoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessondamoaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LessondamoaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/herocorp/lessondamoa/LessondamoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herocorp.lessondamoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessondamoaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
